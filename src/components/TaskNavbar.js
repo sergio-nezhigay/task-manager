dcf6886a98ebe51dc333a2787261d276d7a77ce8
Sergio Nezhigay <abc1971h@gmail.com>
2023-10-11 15:00:07 +0300

    add logo

--- src/components/TaskNavbar.js
+++ src/components/TaskNavbar.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Navbar, Container, Button, Col, Row, Nav } from "react-bootstrap";
+import { Navbar, Container, Button, Nav } from "react-bootstrap";
 import { FilterButtons } from "./FilterButtons";
+import img from "../data/images/icons.svg";
 
 const MyNavbar = ({ openModal }) => {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand>Task manager</Navbar.Brand>
+        <Navbar.Brand className="d-flex align-items-center justify-content-center gap-3">
+          <img src={img} alt="Logo" width="30" height="30" />
+          <span>Task manager</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mx-auto">
