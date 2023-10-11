dd5a78c15e7c57b18a4e8bb7e809b8943b68c314
Sergio Nezhigay <abc1971h@gmail.com>
2023-10-11 16:05:03 +0300

    const correction

--- src/store/constants.js
+++ src/store/constants.js
@@ -8,21 +8,19 @@ export const initialTasks = [
   {
     id: 1,
     name: "Word Play",
-    description:
-      "Create a new task with a title that includes the word 'Banana.'",
+    description: "Create a new task with a title",
     completed: false,
   },
   {
     id: 2,
     name: "Timer Challenge",
-    description:
-      "Set a timer for 30 seconds and complete a task before it runs out",
+    description: "Set a timer for 30 seconds",
     completed: false,
   },
   {
     id: 3,
     name: "Emoji Express",
-    description: "Add an emoji to any existing task",
+    description: "Add an emoji",
     completed: true,
   },
 ];
